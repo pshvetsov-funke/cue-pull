39762c03164b7b912400d0564f7647c492dc8c39
pshvetsov-funke <pavlo.shvetsov@funkemedien.de>
2024-11-12 13:45:49 +0100

    Structure reworked for better modularity. Hashing column added. Schema check added

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import json
 import logging
 import time
@@ -8,7 +7,11 @@ from google.cloud import pubsub_v1
 from google.cloud import bigquery
 from google.api_core.exceptions import DeadlineExceeded
 
-from utils.helpers import process_message, generate_insert_id
+from utils.parser import Parser
+
+# # Debug stuff
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # Create a logger
 logger = logging.getLogger(__name__)
@@ -17,7 +20,7 @@ logger.setLevel(logging.INFO)
 # Initialize clients outside the function scope for better performance
 subscriber_client = pubsub_v1.SubscriberClient()
 bigquery_client = bigquery.Client()
-    
+
 # Environment variables
 PROJECT_ID = os.environ.get('GCP_PROJECT')
 SUBSCRIPTION_NAME = os.environ.get('SUBSCRIPTION_NAME')
@@ -36,33 +39,24 @@ def pull_and_process_messages(request):
 
         subscription_path = subscriber_client.subscription_path(PROJECT_ID, SUBSCRIPTION_NAME)
 
-        max_messages = 1000
-        total_messages_processed = 0
+        parser = Parser(
+            max_iterations=20
+        )
 
-        # Since max messages to pull is limited from GCP side to 1000. This will
-        # ensure that all available messages are pulled (assumed they are 
-        # less that max_messages*max_iterations)
-        max_iterations = 20
-
-        duplicate_count = 0
-        unique_messages = set()
-        total_number_of_received_messages = 0
-        total_number_of_acknowledged_messages = 0
-
-        for iteration in range(max_iterations):
+        for iteration in range(parser.max_iterations):
             logger.info(f"Starting pull iteration {iteration+1}")
             start_time_loc = datetime.now()
             try:
                 response = subscriber_client.pull(
                     request={
                         "subscription": subscription_path,
-                        "max_messages": max_messages,
+                        "max_messages": parser.max_messages,
                     },
                     timeout=5
                 )
                 logger.info(f"Iteration {iteration + 1}: Received "
                             f"{len(response.received_messages)} messages.")
-                total_number_of_received_messages += len(response.received_messages)
+                parser.total_received_messages += len(response.received_messages)
             except DeadlineExceeded:
                 logger.info(f"Pull deadline exceeded. No messages available.")
                 response = -1
@@ -73,58 +67,24 @@ def pull_and_process_messages(request):
                 logger.info("No messages available.")
                 break
 
-            ack_ids = []
-            rows_to_insert = []
-            dlq_rows = []
-            insert_ids = []
-
-            logging.info("Start processing messages.")            
+            parser.reset_iteration_info()
+            logging.info("Start processing messages.")
             for received_message in response.received_messages:
-                message_data = received_message.message.data.decode('utf-8')
                 try:
-                    message_dict = json.loads(message_data)
-
-                    message_frozenset = frozenset(message_dict.items())
-                    # Check for duplicates
-                    if message_frozenset in unique_messages:
-                        duplicate_count += 1
-                        ack_ids.append(received_message.ack_id)
-                        continue  # Skip processing this duplicate message
-
-                    unique_messages.add(message_frozenset)
-
-                    processed_message = process_message(message_dict)
-                    rows_to_insert.append(processed_message)
-                    ack_ids.append(received_message.ack_id)
-
-                    # Generate insertId
-                    insert_id = generate_insert_id(processed_message)
-                    insert_ids.append(insert_id)
+                    parser.process_message(received_message)
                 except Exception as e:
                     logger.error(f"Error processing message: {e}")
-                    # Prepare the message for DLQ
-                    dlq_row = {
-                        'message_data': message_data,
-                        'error_message': str(e),
-                        'timestamp': datetime.now(UTC).isoformat(),
-                    }
-                    dlq_rows.append(dlq_row)
-                    # Acknowledge the message to prevent redelivery
-                    ack_ids.append(received_message.ack_id)
+                    parser.add_message_to_dlq(received_message, e)
 
             # Print out summary
-            set_size = sys.getsizeof(unique_messages) + sum(sys.getsizeof(item) for item in unique_messages)
-            logger.info(f"Unique set size: {set_size/1024/1024} MB")
-            logger.info(f"Number of duplicated messages removed: {duplicate_count}")
+            logger.info(f"Number of duplicated messages removed: {parser.duplicate_count}")
 
             # Insert successfully processed messages into BigQuery
-            if rows_to_insert:
+            if parser.iter_rows_to_insert:
                 logger.info("Start inserting messages to BQ")
-
-                for i in range(0, len(rows_to_insert), 100):
-                    #TODO This part should be implemented better
-                    batch = rows_to_insert[i:i + 100]
-                    batch_ids = insert_ids[i:i + 100]
+                insertion_errors = []
+                for i in range(0, len(parser.iter_rows_to_insert), parser.BQI_STEP):
+                    batch = parser.iter_rows_to_insert[i:i + parser.BQI_STEP]
 
                     # Add timestamp
                     for b in batch:
@@ -132,64 +92,77 @@ def pull_and_process_messages(request):
 
                     errors = bigquery_client.insert_rows_json(
                         table=TABLE_ID,
-                        json_rows=batch,
-                        row_ids=batch_ids
+                        json_rows=batch
                     )
                     if errors:
+                        insertion_errors.append(errors)
                         logger.error(f"Errors occurred during batch insertion: {errors}")
                         # Implement retry logic if necessary
                         time.sleep(2) 
 
-                # errors = bigquery_client.insert_rows_json(TABLE_ID, rows_to_insert)
                 logger.info("BQ insertion done!")
-                if not errors:
-                    logger.info(f"Inserted {len(rows_to_insert)} rows into BigQuery table {TABLE_ID}.")
+                if not insertion_errors:
+                    logger.info(f"Inserted {len(parser.iter_rows_to_insert)} rows "#
+                                f"into BigQuery table {TABLE_ID}.")
                 else:
-                    logger.error(f"Errors inserting rows into BigQuery: {errors}")
+                    logger.error(f"Errors inserting rows into BigQuery: {insertion_errors}")
                     # Move failed inserts to DLQ
-                    for i, error in enumerate(errors):
+                    for i, error in enumerate(insertion_errors):
                         dlq_row = {
-                            'message_data': json.dumps(rows_to_insert[i]),
+                            'message_data': json.dumps(parser.iter_rows_to_insert[i]),
                             'error_message': str(error),
                             'timestamp': datetime.now(UTC).isoformat(),
                         }
-                        dlq_rows.append(dlq_row)
+                        parser.iter_dlq_rows.append(dlq_row)
 
             # Insert failed messages into DLQ BigQuery table
-            if dlq_rows:
-                dlq_errors = bigquery_client.insert_rows_json(DLQ_TABLE_ID, dlq_rows)
+            if parser.iter_dlq_rows:
+                insertion_errors = []
+                for i in range(0, len(parser.iter_dlq_rows), parser.BQI_STEP):
+                    batch = parser.iter_dlq_rows[i:i + parser.BQI_STEP]
+                    dlq_errors = bigquery_client.insert_rows_json(
+                        table=DLQ_TABLE_ID,
+                        json_rows=batch
+                    )
+                    if dlq_errors:
+                        insertion_errors.append(dlq_errors)
+                        logger.error(f"Errors occured during inserting DLQ table: {dlq_errors}")
+
+                logger.info("BQ DLQ insertion done!")
                 if not dlq_errors:
-                    logger.info(f"Inserted {len(dlq_rows)} rows into DLQ BigQuery table {DLQ_TABLE_ID}.")
+                    logger.info(f"Inserted {len(parser.iter_dlq_rows)} rows into "
+                                f"DLQ BigQuery table {DLQ_TABLE_ID}.")
                 else:
-                    logger.error(f"Errors inserting rows into DLQ table: {dlq_errors}")
+                    logger.error(f"Errors inserting rows into DLQ table: {insertion_errors}")
 
                     # Add a specific log message for alerting
                     logger.error("DLQ_INSERTION_FAILURE", extra={
-                        'dlq_errors': dlq_errors,
-                        'dlq_rows': dlq_rows
+                        'dlq_errors': insertion_errors,
+                        'dlq_rows': parser.iter_dlq_rows
                     })
 
             # Acknowledge messages
-            total_number_of_acknowledged_messages += len(ack_ids)
-            if ack_ids:
+            parser.total_acknowledged_messages += len(parser.iter_ack_ids)
+            if parser.iter_ack_ids:
                 subscriber_client.acknowledge(
                     request={
                         "subscription": subscription_path,
-                        "ack_ids": ack_ids,
+                        "ack_ids": parser.iter_ack_ids,
                     }
                 )
-                logger.info(f"Acknowledged {len(ack_ids)} messages.")
+                logger.info(f"Acknowledged {len(parser.iter_ack_ids)} messages.")
 
-            total_messages_processed += len(ack_ids)
+            parser.total_messages_processed += len(parser.iter_rows_to_insert)
             end_time_loc = datetime.now()
-            message_diff = total_number_of_acknowledged_messages - total_number_of_received_messages
+            message_diff = (parser.total_acknowledged_messages 
+                            - parser.total_received_messages)
             if message_diff == 0:
                 logger.info("All messages acknowledged")
             else:
                 logger.warning(f"Number of acknowledged - received = {message_diff}")
             logger.info(f"Iteration elapsed time: {end_time_loc - start_time_loc}")
 
-        logger.info(f"Total messages processed: {total_messages_processed}")
+        logger.info(f"Total messages processed: {parser.total_messages_processed}")
         end_time_glob = datetime.now()
         logger.info(f"Total elapsed time: {end_time_glob - start_time_glob}")
         return ('', 200)
---
+++ utils/parser.py
@@ -0,0 +1,141 @@
+import json
+import logging
+import hashlib
+from datetime import datetime, UTC
+from jsonschema import validate, ValidationError
+
+logger = logging.getLogger(__name__)
+
+class Parser:
+    """Class to unite all message parsing functionalities
+    """
+    def __init__(self, max_iterations):
+        self.max_messages = 1000
+        self.total_messages_processed = 0
+        self.total_received_messages = 0
+        self.total_acknowledged_messages = 0
+        self.unique_messages = set()
+        self.duplicate_count = 0
+
+        # Step for BigQuery batch injection. If it is too big - more memory is required.
+        # Step of 100 show itself good for cloud run function instance size of 512 MB
+        self.BQI_STEP = 100
+
+        # Since max messages to pull is limited from GCP side to 1000. This will
+        # ensure that all available messages are pulled (assumed they are 
+        # less that max_messages*max_iterations)
+        self.max_iterations = max_iterations
+
+        # Load json schema for later message validation from file
+        with open('./utils/schema.json', 'r') as schema_file:
+            self.json_schema = json.load(schema_file)
+        
+        if not self.json_schema:
+            logger.critical("JSON schema couldn't be loaded or is empty!")
+            raise ValueError("JSON schema couldn't be loaded or is empty!")
+
+
+    def reset_iteration_info(self):
+        """Reset information that refers to a single iteration
+        """
+        self.iter_ack_ids = []
+        self.iter_rows_to_insert = []
+        self.iter_dlq_rows = []
+
+
+    def process_message(self, received_message):
+        """Functionality for processing the received raw message from PubSub
+
+        Args:
+            received_message (PubSub message): raw message received
+        """
+        message_data = received_message.message.data.decode('utf-8')
+        message_dict = json.loads(message_data)
+
+        # Ensure that the message has expected format
+        try:
+            validate(instance=message_dict, schema=self.json_schema)
+
+            is_duplicate, message_hash = self.deduplicate_message(
+                message_dict, received_message.ack_id
+            )
+            if not is_duplicate:
+                processed_message = self.transform_message(message_dict, message_hash)
+                self.iter_rows_to_insert.append(processed_message)
+                self.iter_ack_ids.append(received_message.ack_id)
+
+        except ValidationError as e:
+            logger.warning(f"The JSON message has invalid schema. Error :{e}")
+            raise ValidationError
+        except Exception as e:
+            logger.warning(f"The error occured during message validation. Error: {e}")
+            raise ValidationError
+
+
+    def add_message_to_dlq(self, received_message, error):
+        """Add the problematic message to dead letter queue.
+
+        In order not to loose message information - store the message in raw
+        format for later processing
+
+        Args:
+            received_message (PubSub message): raw message received
+        """
+        try:
+            message_data = received_message.message.data.decode('utf-8')
+            dlq_row = {
+                'message_data': message_data,
+                'error_message': str(error),
+                'timestamp': datetime.now(UTC).isoformat()
+            }
+        except Exception as e:
+            dlq_row = {
+                'message_data': received_message.message.data,
+                'error_message': f"Upstream error: {str(error)}, downstream error: {str(e)}",
+                'timestamp': datetime.now(UTC).isoformat()
+            }
+
+        self.iter_dlq_rows.append(dlq_row)
+        self.iter_ack_ids.append(received_message.ack_id)
+
+
+    def deduplicate_message(self, message_dict, ack_id):
+        """Encapsulates message deduplication logic. Calculate content hash. Ensure uniqueness.
+
+        Args:
+            message_dict (dict): message
+        """
+        is_duplicate = False
+        message_bytes = json.dumps(message_dict, sort_keys=True).encode('utf-8')
+        message_hash = hashlib.sha256(message_bytes).hexdigest()
+
+        # Check if the message was processed
+        if message_hash in self.unique_messages:
+            self.duplicate_count += 1
+            self.iter_ack_ids.append(ack_id)
+            is_duplicate = True
+            return is_duplicate, message_hash
+
+        # Adds deduplication logic within single cloud run function invocation.
+        # Through unique_messages set the deduplication logic is not perceived between invocations. 
+        self.unique_messages.add(message_hash)
+
+        #TODO optionally add functionality to determine and log the 
+        # content of first occured duplicate pair for debug reasons 
+        # (if duplicates should not occur)
+
+        #TODO optionally add hash as aditional column in BigQuery table to ease the 
+        # message deduplication within DWH.
+
+        return is_duplicate, message_hash
+
+
+    def transform_message(self, message_dict, message_hash):
+        """Performs the message transformation
+
+        Args:
+            message_dict (dict): message
+        """
+        # Some future transformation (if there should be one)
+        message_dict['hash'] = message_hash
+        return message_dict
